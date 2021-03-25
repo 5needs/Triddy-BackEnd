ae5af0264cf12447966e1ce26cda5292c7db12cb
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-03-25 17:56:46 -0500

    Initial Commit

---
+++ src/main/java/edu/eci/ieti/triddy/TriddyApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.triddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriddyApplication.class, args);
+	}
+
+}
