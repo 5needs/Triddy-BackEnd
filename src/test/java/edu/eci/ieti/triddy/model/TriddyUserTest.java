1c7e8982a02b854c97ee2333e024f73e2e228554
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-04-02 14:35:39 -0500

    model test

---
+++ src/test/java/edu/eci/ieti/triddy/model/TriddyUserTest.java
@@ -0,0 +1,32 @@
+package edu.eci.ieti.triddy.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TriddyUserTest {
+    
+    @Test
+    void createUser(){
+        User user = new User("test@mail.com", "tester");
+        assertEquals("tester", user.getName());
+    }
+
+    @Test
+    void validSetMethods(){
+        User user = new User("test@mail.com", "tester");
+        user.setEmail("other@mail.com");
+        user.setName("other");
+        assertEquals("other", user.getName());
+        assertEquals("other@mail.com", user.getEmail());
+    }
+
+    @Test
+    void validToStringMethod(){
+        User user = new User("test@mail.com", "tester");
+        String expected = String.format("User [email= '%s', name= '%s']","test@mail.com", "tester");
+        assertEquals(expected, user.toString());
+    }
+}
