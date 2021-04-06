e3483a0542a4650b5e80130cce0966497ab1e490
JulianGutierritos <julian.gutierrez-v@mail.escuelaing.edu.co>
2021-04-05 20:54:02 -0500

    :sparkles: configuracion y creacion del websocket

---
+++ src/main/java/edu/eci/ieti/triddy/controller/ChatStompController.java
@@ -0,0 +1,26 @@
+package edu.eci.ieti.triddy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import edu.eci.ieti.triddy.model.Message;
+
+@Controller
+public class ChatStompController {
+
+    @Autowired
+    SimpMessagingTemplate msgt;
+
+    @MessageMapping("/chats.{user}")
+    public void handlerNewMessageOfUser(Message message, @DestinationVariable String user){
+        msgt.convertAndSend("stompClient/chats." + user, message);
+    }
+
+    @MessageMapping("/messages.{chat}")
+    public void handlerNewMessageOfChat(Message message, @DestinationVariable String chat){
+        msgt.convertAndSend("stompClient/messages." + chat, message);
+    }
+}
