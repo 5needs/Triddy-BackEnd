d1c5af3f6976346452ee57d1a2ea5308df7281bc
Edyesid <54111499+Edyesid@users.noreply.github.com>
2021-04-06 00:16:21 -0500

    mapping

--- src/main/java/edu/eci/ieti/triddy/controller/ProductController.java
+++ src/main/java/edu/eci/ieti/triddy/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/products")
+@RequestMapping(value = "/api/products")
 public class ProductController {
     @Autowired
     ProductService productService;
