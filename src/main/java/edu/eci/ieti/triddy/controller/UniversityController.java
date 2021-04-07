ccf1a7b8d45510d79783f6738cc7941916dcfd5f
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-04-07 14:40:00 -0500

    logic completed, implementing test

---
+++ src/main/java/edu/eci/ieti/triddy/controller/UniversityController.java
@@ -0,0 +1,65 @@
+package edu.eci.ieti.triddy.controller;
+
+import edu.eci.ieti.triddy.exceptions.TriddyServiceException;
+import edu.eci.ieti.triddy.model.University;
+import edu.eci.ieti.triddy.services.UniversityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(value = "/api")
+public class UniversityController {
+
+    @Autowired
+    UniversityService universityService;
+
+    @GetMapping("/universities/{idUniversity}")
+    public ResponseEntity<?> getStudentsByUniversity(@PathVariable String idUniversity) {
+        try {
+            ArrayList<String> students = universityService.getStudentsByUniversity((idUniversity));
+            return new ResponseEntity<>(students,HttpStatus.ACCEPTED);
+        } catch (TriddyServiceException e) {
+            Logger.getLogger(UniversityController.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/universities/{idUniversity}/{idClient}/{semester}")
+    public ResponseEntity<?> addStudentToUniversity(@PathVariable String idUniversity, @PathVariable String idClient) {
+        universityService.addUniversity(new University(idUniversity),idClient);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/universities/{idUniversity}/{idClient}")
+    public ResponseEntity<?> updateStudentByUniversity(@PathVariable String idUniversity, @PathVariable String idClient) {
+        try {
+            universityService.updateStudentByUniversity(idUniversity,idClient);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } catch (TriddyServiceException e) {
+            Logger.getLogger(UniversityController.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @DeleteMapping("/universities/{idUniversity}")
+    public ResponseEntity<?> deleteUniversityById(@PathVariable String idUniversity) {
+        try {
+            universityService.deleteUniversityById(idUniversity);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } catch (TriddyServiceException e) {
+            Logger.getLogger(UniversityController.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+
+
+}
---
+++ src/main/java/edu/eci/ieti/triddy/model/University.java
@@ -0,0 +1,54 @@
+package edu.eci.ieti.triddy.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class University {
+
+    @Id
+    private String id;
+    private String idUniversity;
+    private long discount;
+    private ArrayList<String> students;
+
+    public University(String idUniversity){
+        this.idUniversity=idUniversity;
+        this.students= new ArrayList<>();
+        this.discount=0;
+    }
+
+    public long getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String university) {
+        if(university.toLowerCase(Locale.ROOT).equals("eci")){
+            this.discount = 5000;
+        }else{
+            this.discount = 1000;
+        }
+    }
+
+    public ArrayList<String> getStudents() {
+        return students;
+    }
+
+    public void setStudents(String student) {
+        this.students.add(student);
+    }
+
+    public String getIdUniversity() {
+        return idUniversity;
+    }
+
+    public void setIdUniversity(String idUniversity) {
+        this.idUniversity = idUniversity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("University[ idUniversity='%s', discount='%s', semester='%s']",idUniversity,discount,students);
+    }
+}
