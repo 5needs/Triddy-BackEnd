a4187611b8fe6bc67863a9f86a122f18305816ad
Edyesid <54111499+Edyesid@users.noreply.github.com>
2021-04-07 00:03:35 -0500

    Update Product.java

--- src/main/java/edu/eci/ieti/triddy/model/Product.java
+++ src/main/java/edu/eci/ieti/triddy/model/Product.java
@@ -1,7 +1,6 @@
 package edu.eci.ieti.triddy.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.UUID;
 
 public class Product {
