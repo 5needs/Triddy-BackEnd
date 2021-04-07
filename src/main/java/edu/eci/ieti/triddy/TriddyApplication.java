c147c792bce222ebdfcb276eb37bd2ec71ab7ff5
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-04-07 17:24:06 -0500

    Login with token

--- src/main/java/edu/eci/ieti/triddy/TriddyApplication.java
+++ src/main/java/edu/eci/ieti/triddy/TriddyApplication.java
@@ -2,6 +2,10 @@ package edu.eci.ieti.triddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import edu.eci.ieti.triddy.config.JwtFilter;
 
 @SpringBootApplication
 public class TriddyApplication {
@@ -9,5 +13,16 @@ public class TriddyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TriddyApplication.class, args);
 	}
+
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter()
+	{
+		FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter( new JwtFilter() );
+		registrationBean.addUrlPatterns( "/api/*" );
+		
+		return registrationBean;
+	}
+
 
 }
---
+++ src/main/java/edu/eci/ieti/triddy/controller/LoginController.java
@@ -0,0 +1,36 @@
+package edu.eci.ieti.triddy.controller;
+
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.eci.ieti.triddy.exceptions.UserNotFoundException;
+import edu.eci.ieti.triddy.model.User;
+import edu.eci.ieti.triddy.services.LoginService;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping
+    public ResponseEntity<String>  login( @RequestBody User login )
+    {   
+        try{
+            return new ResponseEntity<>( loginService.login(login), HttpStatus.ACCEPTED);
+        }catch(ServletException e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+    
+}
