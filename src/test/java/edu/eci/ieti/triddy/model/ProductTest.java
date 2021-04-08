ec33e58c83611cbb454a304b8cb3243db70b74bf
Elan-MarMEn <59963915+Elan-MarMEn@users.noreply.github.com>
2021-04-08 18:59:18 -0500

    Upload Sonar cover

--- src/test/java/edu/eci/ieti/triddy/model/ProductTest.java
+++ src/test/java/edu/eci/ieti/triddy/model/ProductTest.java
@@ -1,17 +1,24 @@
 package edu.eci.ieti.triddy.model;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class ProductTest {
+
+    String[] temp = new String[2];
+    Product product = new Product(temp,"...","nameTest");
+
 
     @Test
     void getId() {
+
     }
 
     @Test
     void setId() {
+
     }
 
     @Test
@@ -24,33 +31,46 @@ class ProductTest {
 
     @Test
     void getPictures() {
+        assertEquals(temp,product.getPictures());
     }
 
     @Test
     void setPictures() {
+        String[] temp2= new String[5];
+        product.setPictures(temp2);
+        assertEquals(temp2.length, product.getPictures().length);
     }
 
     @Test
     void getName() {
+        assertEquals("nameTest",product.getName());
     }
 
     @Test
     void setName() {
+        product.setName("namedTest2");
+        assertEquals("namedTest2",product.getName());
     }
 
     @Test
     void getDescription() {
+        assertEquals("...",product.getDescription());
     }
 
     @Test
     void setDescription() {
+        product.setDescription("Cambio de test");
+        assertEquals("Cambio de test",product.getDescription());
     }
 
     @Test
     void getAvailable() {
+        assertEquals(true,product.getAvailable());
     }
 
     @Test
     void setAvailable() {
+        product.setAvailable(false);
+        assertEquals(false,product.getAvailable());
     }
 }
