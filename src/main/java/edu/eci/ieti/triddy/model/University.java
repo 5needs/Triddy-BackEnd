2be9849d4c095d3a73327b1bcd410e272fc8ce2a
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-04-08 00:13:35 -0500

    ADD MODEL

---
+++ src/main/java/edu/eci/ieti/triddy/model/University.java
@@ -0,0 +1,49 @@
+package edu.eci.ieti.triddy.model;
+
+import org.springframework.data.annotation.Id;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class University {
+
+    @Id
+    private String id;
+    private String idUniversity;
+    private long discount;
+    private ArrayList<String> students;
+
+    public University(String idUniversity){
+        this.idUniversity=idUniversity;
+        this.students= new ArrayList<>();
+        this.discount=0;
+    }
+
+    public long getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String university) {
+        if(university.toLowerCase(Locale.ROOT).equals("eci")){
+            this.discount = 5000;
+        }else{
+            this.discount = 1000;
+        }
+    }
+
+    public ArrayList<String> getStudents() {
+        return students;
+    }
+
+    public void setStudents(String student) {
+        this.students.add(student);
+    }
+
+    public String getIdUniversity() {
+        return idUniversity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("University[ idUniversity='%s', discount='%s', students='%s']",idUniversity,discount,students);
+    }
+}
---
+++ src/test/java/edu/eci/ieti/triddy/controller/UniversityControllerTest.java
@@ -0,0 +1,77 @@
+package edu.eci.ieti.triddy.controller;
+
+import edu.eci.ieti.triddy.model.University;
+import edu.eci.ieti.triddy.repository.UniversityRepository;
+import edu.eci.ieti.triddy.services.UniversityService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest
+public class UniversityControllerTest {
+
+    @Autowired
+    UniversityService universityService;
+
+    @Autowired
+    UniversityRepository universityRepository;
+
+    @Autowired
+    UniversityController universityController;
+
+    @AfterEach
+    void deletePayments(){
+        universityRepository.deleteAll();
+    }
+
+    @Test
+    void addStudentToUniversity(){
+        ResponseEntity<?> response = universityController.addStudentToUniversity("ECI","1223");
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    void getStudentsByUniversity(){
+        universityService.addUniversity(new University("ECI"),"12237");
+        ResponseEntity<?> response = universityController.getStudentsByUniversity("ECI");
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    }
+
+    @Test
+    void shouldNotGetStudentsByUniversity(){
+        ResponseEntity<?> response = universityController.getStudentsByUniversity("ECIfgsdfasd");
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
+    @Test
+    void updateStudentByUniversity(){
+        universityService.addUniversity(new University("ECI"),"12237");
+        ResponseEntity<?> response = universityController.updateStudentByUniversity("ECI","12237");
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    }
+
+    @Test
+    void shouldNotPpdateStudentByUniversity(){
+        universityService.addUniversity(new University("ECI"),"12237");
+        ResponseEntity<?> response = universityController.updateStudentByUniversity("ECI456745674567","12237");
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
+    @Test
+    void deleteUniversityById(){
+        universityService.addUniversity(new University("ECI"),"12237");
+        ResponseEntity<?> response = universityController.deleteUniversityById("ECI");
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    }
+
+    @Test
+    void shouldNotDeleteUniversityById(){
+        ResponseEntity<?> response = universityController.deleteUniversityById("ECI6745674567");
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
+}
