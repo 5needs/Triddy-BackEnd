f428bf123715588f2ab9155391183750d64ad850
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-04-07 20:30:25 -0500

    config

--- src/main/java/edu/eci/ieti/triddy/config/JwtFilter.java
+++ src/main/java/edu/eci/ieti/triddy/config/JwtFilter.java
@@ -13,50 +13,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * @author Santiago Carrillo
- * 8/21/17.
- */
-public class JwtFilter
-    extends GenericFilterBean
-{
+public class JwtFilter extends GenericFilterBean{
 
-    public void doFilter( final ServletRequest servletRequest, final ServletResponse servletResponse,
-                          final FilterChain filterChain )
-        throws IOException, ServletException
-    {
+    public void doFilter( final ServletRequest servletRequest, final ServletResponse servletResponse,final FilterChain filterChain ) throws IOException{
 
         final HttpServletRequest request = (HttpServletRequest) servletRequest;
         final HttpServletResponse response = (HttpServletResponse) servletResponse;
         final String authHeader = request.getHeader( "authorization" );
-
-        if ( "OPTIONS".equals( request.getMethod() ) )
-        {
-            response.setStatus( HttpServletResponse.SC_OK );
-
-            filterChain.doFilter( servletRequest, response );
-        }
-        else
-        {
-
-            if ( authHeader == null || !authHeader.startsWith( "Bearer " ) )
-            {
-                throw new ServletException( "Missing or invalid Authorization header" );
+        try{
+            if ( "OPTIONS".equals( request.getMethod() ) ){
+                response.setStatus( HttpServletResponse.SC_OK );
+                filterChain.doFilter( servletRequest, response );
             }
+            else{
 
-            final String token = authHeader.substring( 7 );
+                if ( authHeader == null || !authHeader.startsWith( "Bearer " ) ){
+                    throw new ServletException( "Missing or invalid Authorization header" );
+                }
 
-            try
-            {
-                final Claims claims = Jwts.parser().setSigningKey( "secretkey" ).parseClaimsJws( token ).getBody();
-                request.setAttribute( "claims", claims );
+                final String token = authHeader.substring( 7 );
+
+                try{
+                    final Claims claims = Jwts.parser().setSigningKey( "secretkey" ).parseClaimsJws( token ).getBody();
+                    request.setAttribute( "claims", claims );
+                }
+                catch ( final SignatureException e ){
+                    throw new ServletException( "Invalid token" );
+                }
+
+                filterChain.doFilter( servletRequest, response );
             }
-            catch ( final SignatureException e )
-            {
-                throw new ServletException( "Invalid token" );
-            }
-
-            filterChain.doFilter( servletRequest, response );
+        }catch (ServletException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(e.getMessage());
+            response.getWriter().flush();
         }
     }
 }
