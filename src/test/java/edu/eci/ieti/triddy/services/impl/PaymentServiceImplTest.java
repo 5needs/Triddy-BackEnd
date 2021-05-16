e4bee47593bb6f760bd3b1653c3eddec4403cd4e
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-05-16 11:53:16 -0500

    refactor name class

--- src/test/java/edu/eci/ieti/triddy/services/impl/PaymentServiceImplTest.java
+++ src/test/java/edu/eci/ieti/triddy/services/impl/PaymentServiceImplTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class UserServiceImplTest  {
+public class PaymentServiceImplTest {
 
     @Autowired
     PaymentService paymentService;
