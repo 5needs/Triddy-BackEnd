54f6c65bea4f172ec721a582dad1cb2ca4d00efd
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-05-21 09:35:55 -0500

    add test service impl

--- src/main/java/edu/eci/ieti/triddy/services/impl/PointsServiceImpl.java
+++ src/main/java/edu/eci/ieti/triddy/services/impl/PointsServiceImpl.java
@@ -1,7 +1,9 @@
 package edu.eci.ieti.triddy.services.Impl;
 
 import edu.eci.ieti.triddy.services.PointsService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PointsServiceImpl implements PointsService {
     @Override
     public void addPoints(String points) {
